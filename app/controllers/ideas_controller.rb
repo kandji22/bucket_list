5810a8f10f54dddf0fb7d085b5ffddaf70ac0a7b
kandji22 <Logement@1990>
2019-10-15 07:34:05 +0200

    creation de create et verification de la valeur titles

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,12 +1,16 @@
 class IdeasController < ApplicationController
   def index
     @search_term = params[:q]
-    logger.info("le theme de recherche es #{@search_term} ")
+    logger.info("le theme de recherche est #{@search_term} ")
   end
 
   def new
+
   end
 
   def create
+    @title = params[:title]
+    logger.info("le theme de recherche est #{@title} ")
+    redirect_to(ideas_index_path)
   end
 end
