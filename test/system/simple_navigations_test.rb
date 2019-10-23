743d6b08b7a37ef284a594ea99700c5c4d6aa743
kandji22 <Logement@1990>
2019-10-23 08:55:26 +0200

    creation de fichier test ideas

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,11 +1,39 @@
 require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
-  test 'style guide navigation' do
+
+  test 'search term is displayed' do
     visit('/')
     assert page.has_content?('Bucket List')
     fill_in('q', with: 'Spain').send_keys(:enter)
     assert has_content?('Spain')
     assert current_url.include?('q=Spain')
+  end
+
+
+  test 'style guide navigation' do
+    visit('/')
+    sleep(5.seconds)
+    click_on('Style Guide',match: :first)
+    sleep(5.seconds)
+    assert page.has_content?("Atoms")
+    sleep(5.seconds)
+    assert page.has_content?("Molecules")
+    sleep(5.seconds)
+    assert page.has_content?("Organisms")
+    sleep(5.seconds)
+    click_on('Molecules',match: :first)
+    sleep(5.seconds)
+    assert page.has_content?("Card")
+    sleep(5.seconds)
+    click_on('Organisms', match: :first)
+    sleep(5.seconds)
+    assert page.has_content?('Create goal')
+    sleep(5.seconds)
+    click_on('My Bucket List', match: :first)
+    sleep(5.seconds)
+    assert page.has_content?('Some ideas')
+
+
   end
 end
