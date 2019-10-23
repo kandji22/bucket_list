6facee54ecc86be4ef2afd801b8cfedf967664b7
kandji22 <Logement@1990>
2019-10-23 07:33:49 +0200

    remplacez le test de stub par le search term is displayed

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit simple_navigations_url
-  #
-  #   assert_selector "h1", text: "SimpleNavigation"
-  # end
+  test 'search term is displayed' do
+    visit('/')
+    assert page.has_content?('Bucket List')
+    fill_in('q', with: 'Spain').send_keys(:enter)
+    assert has_content?('Spain')
+    assert current_url.include?('q=Spain')
+  end
 end
