e28c7acb7b3d539a36254333a46c51b5759f94fd
kandji22 <Logement@1990>
2019-10-23 09:20:11 +0200

    ajout test d une nouvelle idée et verification d existance dans la base de donné

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -1,9 +1,18 @@
 require "application_system_test_case"
 
 class IdeasTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit ideas_url
-  #
-  #   assert_selector "h1", text: "Ideas"
-  # end
+  test 'create new idea' do
+    visit ideas_new_path
+    sleep(2.seconds)
+    fill_in('title', with: 'ma premier test')
+      sleep(2.seconds)
+    fill_in('nbr d abonne', with: 30)
+      sleep(2.seconds)
+    fill_in('photo url', with: 'http://fpoimg.com/255x170')
+      sleep(2.seconds)
+    click_on('Create')
+    sleep(2.seconds)
+    visit acount_ideas_path
+    sleep(2.seconds)
+  end
 end
