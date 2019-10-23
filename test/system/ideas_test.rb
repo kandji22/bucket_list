05059a1c4a5839d1a2ae0dfe1591be7693ce2e9a
kandji22 <Logement@1990>
2019-10-23 13:46:09 +0200

    creation de donné dans base de donné puis affiché sur la page test

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -12,7 +12,24 @@ class IdeasTest < ApplicationSystemTestCase
       sleep(2.seconds)
     click_on('Create')
     sleep(2.seconds)
+    assert page.has_content?('ma premier test')
+    sleep(5.seconds)
+  end
+  test ' Ideas records' do
+    idea1=Idea.new
+    idea1.title='titre enregistrer avans le test'
+    idea1.done_count=25
+    idea1.photo_url='http://fpoimg.com/255x170'
+    idea1.save!
+    idea2=Idea.new
+    idea2.title='titre2 enregistrer avans le test2'
+    idea2.done_count=50
+    idea2.photo_url='http://fpoimg.com/255x170'
+    idea2.save!
     visit acount_ideas_path
-    sleep(2.seconds)
+    sleep(5.seconds)
+    assert page.has_content?('ma premier test')
+    sleep(5.seconds)
+
   end
 end
