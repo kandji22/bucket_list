136443e8156533dd9e674714400c5aa240ebe6c0
kandji22 <Logement@1990>
2019-10-30 13:49:58 +0100

    erreur test numero 6 réglé

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -3,17 +3,11 @@ require "application_system_test_case"
 class IdeasTest < ApplicationSystemTestCase
   test 'create new idea' do
     visit ideas_new_path
-    sleep(2.seconds)
     fill_in('title', with: 'ma premier test')
-      sleep(2.seconds)
     fill_in('nbr d abonne', with: 30)
-      sleep(2.seconds)
     fill_in('photo url', with: 'http://fpoimg.com/255x170')
-      sleep(2.seconds)
     click_on('Create')
-    sleep(2.seconds)
     assert page.has_content?('ma premier test')
-    sleep(5.seconds)
   end
   test ' Ideas records' do
     idea1=Idea.new
@@ -27,9 +21,7 @@ class IdeasTest < ApplicationSystemTestCase
     idea2.photo_url='http://fpoimg.com/255x170'
     idea2.save!
     visit acount_ideas_path
-    sleep(5.seconds)
     assert page.has_content?('ma premier test')
-    sleep(5.seconds)
 
   end
 end
