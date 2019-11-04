70da8761797840b7018ef150219244e2ee5ced72
kandji22 <Logement@1990>
2019-11-04 19:41:39 +0100

    creation de validation d exigence de titre

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,11 @@
  class Idea < ApplicationRecord
+
+validates :title, presence: true
+
   def self.search(search_term)
 where('title LIKE?',"%#{search_term}%").or(where('migration LIKE?', "%#{search_term}%"))
 end
+
 def self.most_recent()
   all.order(created_at: :desc).limit(3)
 end
