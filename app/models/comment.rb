66064c7ef6967703c36fa20ccac556e11bf5446a
kandji22 <Logement@1990>
2019-11-06 16:28:24 +0100

    creation de cotrolleur user et view new et create

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :idea
   belongs_to :user
-  validates :user, presence: true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   get 'home/index'
 
   get 'styles/atoms'
